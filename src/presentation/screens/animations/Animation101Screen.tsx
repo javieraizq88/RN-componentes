1cfb33027fb176c0d3b569961f0682d2707c829b
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2024-07-18 18:37:34 -0400

    animacion de entrada y de salida con Easing.poly funcionando

--- src/presentation/screens/animations/Animation101Screen.tsx
+++ src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,26 +1,35 @@
-import { Animated, Pressable, StyleSheet, Text, View } from "react-native"
+import { Animated, Easing, Pressable, StyleSheet, Text, View } from "react-native"
 import { colors } from "../../../config/theme/theme"
 import { useRef } from "react"
 
 export const Animation101Screen = () => {
 
     const animatedOpacity = useRef(new Animated.Value(0.4)).current;
+    const animatedTop = useRef(new Animated.Value(-100)).current;
 
-const fadeIn = () => {
-    Animated.timing( animatedOpacity, {
-        toValue: 1,
-        duration: 300,
-        useNativeDriver: true,
-    }).start( () =>console.log("animated ended"))
-}
 
-const fadeout = () => {
-    Animated.timing( animatedOpacity, {
-        toValue: 0,
-        duration: 300,
-        useNativeDriver: true,
-    }).start( () =>console.log("animated start"))
-}
+    const fadeIn = () => {
+        Animated.timing(animatedOpacity, {
+            toValue: 1,
+            duration: 300,
+            useNativeDriver: true,
+        }).start(() => console.log("animated ended"));
+
+        Animated.timing(animatedTop, {
+            toValue: 0,
+            duration: 700,
+            useNativeDriver: true,
+            easing: Easing.poly(1),
+        }).start(() => console.log("animated ended"));
+    };
+
+    const fadeOut = () => {
+        Animated.timing(animatedOpacity, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: true,
+        }).start(() => animatedTop.resetAnimation());
+    }
 
     return (
         <View style={styles.container} >
@@ -28,17 +37,20 @@ const fadeout = () => {
             <Animated.View style={[
                 styles.purpleBox,
                 {
-                    opacity: animatedOpacity
+                    opacity: animatedOpacity,
+                    transform:  [{
+                        translateY: animatedTop
+                    }]
                 }
             ]} />
 
 
 
-            <Pressable onPress={fadeIn } style={{ marginTop: 10 }} >
+            <Pressable onPress={fadeIn} style={{ marginTop: 10 }} >
                 <Text>FadeIn</Text>
             </Pressable>
 
-            <Pressable onPress={ fadeout } style={{ marginTop: 10 }} >
+            <Pressable onPress={fadeOut} style={{ marginTop: 10 }} >
                 <Text>Fadeout</Text>
             </Pressable>
 
