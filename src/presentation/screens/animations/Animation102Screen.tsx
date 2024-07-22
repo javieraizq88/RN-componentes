d41f47a4e1c3eb7409e93abe36bc12f92cd5e347
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2024-07-22 13:18:09 -0400

    animated value x,y

---
+++ src/presentation/screens/animations/Animation102Screen.tsx
@@ -0,0 +1,31 @@
+
+import {Animated, PanResponder, StyleSheet, View} from 'react-native';
+
+export const Animation102Screen = () => {
+
+
+    return (
+        <View style={styles.container}>
+      
+
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    box: {
+      backgroundColor: '#61dafb',
+      width: 80,
+      height: 80,
+      borderRadius: 4,
+    },
+  });
+
+function useRef(arg0: any) {
+    throw new Error("Function not implemented.");
+}
