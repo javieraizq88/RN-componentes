84c559f0afb89456dd562cfde774b2eea19a8346
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2024-07-22 16:12:31 -0400

    scroll y teclado

--- src/presentation/screens/inputs/TextInputScreen.tsx
+++ src/presentation/screens/inputs/TextInputScreen.tsx
@@ -1,9 +1,10 @@
-import { Text, TextInput, View } from "react-native"
+import { KeyboardAvoidingView, Platform, Text, TextInput, View } from "react-native"
 import { CustomView } from "../../components/ui/CustomView"
 import { Titulo } from "../../components/ui/Titulo"
 import { Card } from "../../components/ui/Card"
 import { globalStyles } from "../../../config/theme/theme"
 import { useState } from "react"
+import { ScrollView } from "react-native-gesture-handler"
 
 export const TextInputScreen = () => {
 
@@ -14,41 +15,53 @@ export const TextInputScreen = () => {
     })
 
     return (
-        <CustomView margin >
-            <Titulo safe text="Text inputs" />
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : undefined}>  {/* esconde el teclado en IOS */}
+            <ScrollView>
 
-            <Card>
-                <TextInput
-                    style={globalStyles.input}
-                    placeholder="Nombre"
-                    autoCapitalize={"words"}
-                    autoCorrect={false}
-                    onChangeText={ value => setForm({ ...form, name: value }) }
-                />
+                <CustomView margin >
+                    <Titulo safe text="Text inputs" />
 
-<TextInput
-                    style={globalStyles.input}
-                    placeholder="correo"
-                    autoCapitalize={"none"}
-                    autoCorrect={ false }
-                    keyboardType="email-address"
-                    onChangeText={ value => setForm({ ...form, email: value }) }
-                />
+                    <Card>
+                        <TextInput
+                            style={globalStyles.input}
+                            placeholder="Nombre"
+                            autoCapitalize={"words"}
+                            autoCorrect={false}
+                            onChangeText={value => setForm({ ...form, name: value })}
+                        />
 
-<TextInput
-                    style={globalStyles.input}
-                    placeholder="telefono"
-                    keyboardType="phone-pad"
-                    onChangeText={ value => setForm({ ...form, phone: value }) }
-                />
-            </Card>
+                        <TextInput
+                            style={globalStyles.input}
+                            placeholder="correo"
+                            autoCapitalize={"none"}
+                            autoCorrect={false}
+                            keyboardType="email-address"
+                            onChangeText={value => setForm({ ...form, email: value })}
+                        />
 
-            <View style={{ height: 10 }} />
+                        <TextInput
+                            style={globalStyles.input}
+                            placeholder="telefono"
+                            keyboardType="phone-pad"
+                            onChangeText={value => setForm({ ...form, phone: value })}
+                        />
+                    </Card>
 
-            <Card>
-                <Text>{JSON.stringify(form, null, 2)} </Text>
-            </Card>
+                    <View style={{ height: 10 }} />
 
-        </CustomView>
+                    <Card>
+                        <Text>{JSON.stringify(form, null, 2)} </Text>
+                        <Text>{JSON.stringify(form, null, 2)} </Text>
+                        <Text>{JSON.stringify(form, null, 2)} </Text>
+                        <Text>{JSON.stringify(form, null, 2)} </Text>
+                        <Text>{JSON.stringify(form, null, 2)} </Text>
+                        <Text>{JSON.stringify(form, null, 2)} </Text>
+                    </Card>
+                </CustomView>
+
+                <View style={{ height: 40 }} />
+
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
