856fab36155524c1d9b1a2af63c0aa152cf4e814
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2024-07-23 16:50:01 -0400

    theme context y cambio en el app

--- App.tsx
+++ App.tsx
@@ -2,13 +2,24 @@
 import 'react-native-gesture-handler';
 
 import { NavigationContainer } from "@react-navigation/native";
-import { Text, View } from "react-native";
 import { Navigator } from './src/presentation/navigator/Navigator';
+import { PropsWithChildren } from 'react';
+import { ThemeProvider } from './src/presentation/context/ThemeContext';
+
+const AppState = ({ children }: PropsWithChildren) => {
+  return (
+    <NavigationContainer>
+      <ThemeProvider>
+        {children}
+      </ThemeProvider>
+    </NavigationContainer>
+  )
+}
 
 export const App = () => {
   return (
-    <NavigationContainer>
+    <AppState>
       <Navigator />
-    </NavigationContainer>
+    </AppState>
   )
 }
