43ec7b8767298caf9303fffc5285f966b43853d2
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2024-07-24 11:43:22 -0400

    app lista con theme dark y light

--- App.tsx
+++ App.tsx
@@ -1,39 +1,18 @@
 // Only import react-native-gesture-handler on native platforms
 import 'react-native-gesture-handler';
 
-import { DarkTheme, DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { Navigator } from './src/presentation/navigator/Navigator';
-import { PropsWithChildren, useContext } from 'react';
-import { ThemeContext, ThemeProvider } from './src/presentation/context/ThemeContext';
+import { ThemeProvider } from './src/presentation/context/ThemeContext';
 
 
 // el useContext puede ser llamado desde el AppNavigation 
 // se separo para el children tenga acceso al AppTheme
 
-const AppNavigation = ({ children }: PropsWithChildren) => {
-  const { isDark } = useContext(ThemeContext);
-
-  return (
-    <NavigationContainer theme={ isDark ? DarkTheme : DefaultTheme }>
-      {children}
-    </NavigationContainer>
-  )
-}
-
-const AppTheme = ({ children }: PropsWithChildren) => {
-  return (
-    <ThemeProvider>
-      <AppNavigation>
-        {children}
-      </AppNavigation>
-    </ThemeProvider>
-  )
-}
 
 export const App = () => {
   return (
-    <AppTheme>
+    <ThemeProvider>
       <Navigator />
-    </AppTheme>
+    </ThemeProvider>
   )
 }
